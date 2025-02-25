ba02213a8612111e1a060b762868bf6d7231e6e7
CygnusX-26 <qh4nveh@gmail.com>
2025-02-25 01:31:24 -0500

    improve dimension scaling

--- cards/TeamCard.ts
+++ cards/TeamCard.ts
@@ -5,7 +5,7 @@ export enum Theme {
     DARK
 }
 
-interface Dimensions {
+export interface Dimensions {
     width: number,
     height: number,
     duration: number,
@@ -59,15 +59,15 @@ export class TeamCard {
             country_percentage = this.country_rating / 200;
         }
 
-        const global_bar_fill: number = Math.floor((1 - global_percentage) * 300);
-        const country_bar_fill: number = Math.floor((1 - country_percentage) * 300);
+        const global_bar_fill: number = Math.floor((1 - global_percentage) * 75);
+        const country_bar_fill: number = Math.floor((1 - country_percentage) * 75);
 
         const logo_b64: string = this.logo.length > 0 ? await image_to_base64(this.logo) : "";
-        const country_flag = this.country.length > 0 ? await image_to_svg(`https://ctftime.org/static/images/sf/${this.country.toLowerCase()}.svg`): "";
-        const ctftime_logo = await get_ctftime_logo();
+        const country_flag: string = this.country.length > 0 ? await image_to_svg(`https://ctftime.org/static/images/sf/${this.country.toLowerCase()}.svg`): "";
+        const ctftime_logo: string = await get_ctftime_logo();
 
-        let logo_str: string = this.logo.length > 0 ? `<image x="320" y="20" width="60" height="60" href="data:image/png;base64,${logo_b64}"/>`: "";
-        let country_str: string = this.country.length > 0 ? `<image x="100" y="110" width="20" height="20" href="data:image/svg+xml;utf8,${country_flag}"/>` : "";
+        let logo_str: string = this.logo.length > 0 ? `<image x="80%" y="10%" width="15%" height="30%" href="data:image/png;base64,${logo_b64}"/>`: "";
+        let country_str: string = this.country.length > 0 ? `<image x="26%" y="55%" width="5%" height="10%" href="data:image/svg+xml;utf8,${country_flag}"/>` : "";
 
         let bg_color: string = "#ffffff";
         let text_color: string = "#000000";
@@ -79,6 +79,10 @@ export class TeamCard {
             text_color = "#ffffff";
             bar_color_filled = "#0d74e7";
             bar_color_unfilled = "#e4e4e4";
+        }
+
+        const fit_to_dimension = (value: number): number => {
+            return value;
         }
 
         return `<svg width="${this.dimensions.width.toString()}" height="${this.dimensions.height.toString()}" viewBox="0 0 ${this.dimensions.width.toString()} ${this.dimensions.height.toString()}" xmlns="http://www.w3.org/2000/svg" fill="none">
@@ -102,41 +106,46 @@ export class TeamCard {
                 to { opacity: 1; }
             }
         </style>
-        <rect x="0" y="0" width="${this.dimensions.width.toString()}" height="${this.dimensions.height.toString()}" rx="7.5" fill="${bg_color}" stroke="#e1e4e8" stroke-width="2"/>
-        <text x="20" y="50" font-size="30" font-weight="bold" id="team-name" fill="${text_color}">${this.name}</text>
+        <rect x="0%" y="0%" width="100%" height="100%" rx="7.5" fill="${bg_color}" stroke="#e1e4e8" stroke-width="2"/>
+        <text x="5%" y="25%" font-size="2em" font-weight="bold" id="team-name" fill="${text_color}">${this.name}</text>
         <g class="fade-in">
         ${logo_str}
 
-        <text x="20" y="90" font-size="13" fill="${text_color}">Global rank</text>
-        <rect x="20" y="100" width="300" height="5" rx="2" fill="${bar_color_unfilled}"/>
+        <text x="5%" y="45%" font-size="0.83em" fill="${text_color}">Global rank</text>
+        <rect x="5%" y="50%" width="75%" height="2.5%" rx="2" fill="${bar_color_unfilled}"/>
 
-        <rect id="progress-bar" x="20" y="100" width="0" height="5" rx="2" fill="${bar_color_filled}">
-            <animate attributeName="width" from="0" to="${global_bar_fill}" dur="${this.dimensions.duration.toString()}s" fill="freeze"/>
+        <rect id="progress-bar" x="5%" y="50%" width="0%" height="2.5%" rx="2" fill="${bar_color_filled}">
+            <animate attributeName="width" from="0%" to="${global_bar_fill}%" dur="${this.dimensions.duration.toString()}s" fill="freeze"/>
         </rect>
-        <text id="progress-text" x="330" y="106" font-size="12" fill="${text_color}">${!isNaN(this.global_rating) ? this.global_rating.toString(): "N/A"}</text>
+        <text id="progress-text" x="82.5%" y="53%" font-size="0.8em" fill="${text_color}">${!isNaN(this.global_rating) ? this.global_rating.toString(): "N/A"}</text>
         
         
-        <text x="20" y="125" font-size="13" fill="${text_color}">Country rank</text>
+        <text x="5%" y="62.5%" font-size="0.83em" fill="${text_color}">Country rank</text>
         ${country_str}
-        <rect x="20" y="135" width="300" height="5" rx="2" fill="${bar_color_unfilled}"/>
+        <rect x="5%" y="67.5%" width="75%" height="2.5%" rx="2" fill="${bar_color_unfilled}"/>
 
-        <rect id="progress-bar" x="20" y="135" width="0" height="5" rx="2" fill="${bar_color_filled}">
-            <animate attributeName="width" from="0" to="${country_bar_fill}" dur="${this.dimensions.duration.toString()}s" fill="freeze"/>
+        <rect id="progress-bar" x="5%" y="67.5%" width="0%" height="2.5%" rx="2" fill="${bar_color_filled}">
+            <animate attributeName="width" from="0%" to="${country_bar_fill}%" dur="${this.dimensions.duration.toString()}s" fill="freeze"/>
         </rect>
-        <text id="progress-text" x="330" y="141" font-size="12" fill="${text_color}">${!isNaN(this.country_rating) ? this.country_rating.toString(): "N/A"}</text>
+        <text id="progress-text" x="82.5%" y="70.5%" font-size="0.8em" fill="${text_color}">${!isNaN(this.country_rating) ? this.country_rating.toString(): "N/A"}</text>
         </g>
-        <text x="20" y="180" font-size="10" fill="#6a737d">Powered by </text>
-        <image x="75" y="157" width="40" height="40" href="data:image/svg+xml;utf8,${ctftime_logo}"/>
+        <text x="5%" y="90%" font-size="0.66em" fill="#6a737d">Powered by </text>
+        <image x="18.75%" y="78.5%" width="10%" height="20%" href="data:image/svg+xml;utf8,${ctftime_logo}"/>
         <script>
-            const text = document.getElementById("team-name");
-            
-            let bbox = text.getBBox();
-            let maxWidth = 300;
+            function scaleText() {
+                const text = document.getElementById("team-name");
+                const maxWidth = ${this.dimensions.width.toString()} * 0.75;
+                if (text.getBBox().width > maxWidth) {
+                    const bbox = text.getBBox(); // Get text dimensions
+                    const scale = maxWidth / bbox.width;
+                    const cx = 0;
+                    const cy = bbox.y + bbox.height / 2;
 
-            if (bbox.width > maxWidth) {
-                let scale = maxWidth / bbox.width;
-                text.setAttribute("transform", \`scale(\$\{scale\}, 1)\`);
+                    text.setAttribute("transform", \`translate(\$\{cx\}, \$\{cy\}) scale(\$\{scale\}) translate(\$\{-cx\}, \$\{-cy\})\`);
+                } 
             }
+            window.addEventListener("resize", scaleText);
+            window.addEventListener("load", scaleText);
         </script>
     </svg>`;
     }
--- index.ts
+++ index.ts
@@ -1,5 +1,5 @@
 import { get_team, escape_xml } from "./utils/utils.js"
-import { TeamCard, Theme } from "./cards/TeamCard.js"
+import { TeamCard, Theme, Dimensions } from "./cards/TeamCard.js"
 import { error_svg } from "./utils/error.js";
 import express, { Request, Response } from 'express';
 
@@ -14,6 +14,8 @@ app.use((req, res, next) => {
 app.get('/', async (req: Request, res: Response) => {
     const teamid: string = req.query.teamid as string;
     const year: number = new Date().getFullYear();
+    let x: string = req.query.x as string;
+    let y: string = req.query.y as string;
     let theme: Theme = Theme.LIGHT;
     res.set('Content-Type', 'image/svg+xml');
 
@@ -26,6 +28,15 @@ app.get('/', async (req: Request, res: Response) => {
         theme = Theme.DARK;
     }
 
+    let x_dim = 400;
+    if (x && parseInt(x)) {
+        x_dim = parseInt(x);
+    }
+    let y_dim = 200;
+    if (y && parseInt(y)) {
+        y_dim = parseInt(y);
+    }
+
     (get_team(parseInt(teamid))).then((data): void => {
         new TeamCard(
             escape_xml(data.name),
@@ -33,7 +44,8 @@ app.get('/', async (req: Request, res: Response) => {
             escape_xml(data.country),
             data.rating[year].country_place,
             data.rating[year].rating_place,
-            theme).render_svg().then((svg) => {
+            theme,
+            {width: x_dim, height: y_dim, duration: 2}).render_svg().then((svg) => {
                 res.send(svg);
             })
         return;
